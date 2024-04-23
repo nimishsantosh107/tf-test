8c86074fc42d4ef2938e9a853aa9f388acd249de
nimishsantosh107 <36289353+nimishsantosh107@users.noreply.github.com>
2024-04-23 12:23:04 +0530

    limited localStorage arr length to 10

--- src/services/localStorage.ts
+++ src/services/localStorage.ts
@@ -10,6 +10,7 @@ export const appendLSTokenData = (item: string) => {
     if (!data.includes(item)) {
         // push to start of array
         data.unshift(item);
+        if (data.length > 10) data = data.slice(0, 10);
         localStorage.setItem(LS_KEY, JSON.stringify(data));
     }
 };
